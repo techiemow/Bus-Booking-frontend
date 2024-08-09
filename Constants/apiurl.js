43033a44b1102acc32a5da2410646318a8fdd687
techiemow <mowriyaabk@gmail.com>
2024-08-09 19:08:09 +0530

    backend api added

--- Constants/apiurl.js
+++ Constants/apiurl.js
@@ -1,4 +1,4 @@
 export const apiurl =
   import.meta.env.VITE_LOCAL_URL === "production"
-    ? "http://localhost:4000"
+    ? "https://bus-booking-backend-8g3m.onrender.com"
     : `http://localhost:4000`;
